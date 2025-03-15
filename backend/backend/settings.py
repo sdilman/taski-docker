7a179a007b0add49cb54524c2415c11b7aec5c04
Stepan Dilman <stepan.dilman@gmail.com>
2025-03-15 19:06:14 +0500

    Update DB credentials

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB', 'django_db'),
         'USER': os.getenv('POSTGRES_USER', 'django_user'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'django_password'),
-        'HOST': os.getenv('DB_HOST', ''),
+        'HOST': os.getenv('DB_HOST', '127.0.0.1'),
         'PORT': os.getenv('DB_PORT', 5432)
     }
 }
